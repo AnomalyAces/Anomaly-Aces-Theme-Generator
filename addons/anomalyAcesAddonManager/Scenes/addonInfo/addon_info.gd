4c6b5ea3bc6b00fcd214a30804e7baaf4d7aebee
Anomaly Aces <anomalyaces@gmail.com>
2026-07-03 17:47:59 -0400

    removing files that should have been ignored

--- addons/anomalyAcesAddonManager/Scenes/addonInfo/addon_info.gd
+++
@@ -1,6 +0,0 @@
-@tool
-extends HBoxContainer
-
-
-func _ready() -> void:
-	alignment = BoxContainer.ALIGNMENT_CENTER
--- addons/anomalyAcesAddonManager/Scripts/AddonManagerUtil/Models/GithubPATInfo.gd
+++
@@ -1,5 +0,0 @@
-@tool
-class_name GithubPATInfo extends Object
-
-var token: String
-var expiration_date: String
